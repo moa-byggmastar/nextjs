3fd7af2cbdf6f170d744c27dc39572009633daff
moa-byggmastar <moabygg07@gmail.com>
2025-01-21 10:23:19 +0200

    ?

---
+++ app/register/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Image from 'next/image'
+
+export default function Register() {
+    return (
+        <div>
+            <h1 className='rubrik'>Register</h1>
+            <Image src={'/primo.jpeg'} width={50} height={50} alt="" />
+        </div>
+    )
+}
