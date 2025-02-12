c10ca9cf53dcb9c9dbbb0d2579eb6a308493612b
moa-byggmastar <moabygg07@gmail.com>
2025-02-12 09:19:46 +0200

    .

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,8 +5,11 @@ export default function Navbar() {
     return (
         <div>
             <ul>
+                <li><Link href="/">Home</Link></li>
                 <li><Link href="/about">About</Link></li>
                 <li><Link href="/contact">Contact</Link></li>
+                <li><Link href="/form">Form</Link></li>
+                <li><Link href="/gissatalet">Guess Number</Link></li>
             </ul>
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,7 @@ export default function page() {
   return (
     <div className='start-body'>
       <div className='start-screen'>
-        <h1 className='instructions'>För Gissa Talet, lägg till /gissatalet i url</h1>
-        <h1 className='instructions'>För Formulär, lägg till /form i url</h1>
+        <h1>Welcome!</h1>
       </div>
     </div>
   )
